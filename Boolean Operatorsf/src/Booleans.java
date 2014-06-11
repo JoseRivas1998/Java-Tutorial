15418b5918fda0d9cbe0fc08dd4294f1a9833872
JoseRivas1998 <JoseRivas823@gmail.com>
2014-06-11 16:22:35 -0700

    First Commit

---
+++ Boolean Operatorsf/src/Booleans.java
@@ -0,0 +1,19 @@
+
+public class Booleans {
+	public static boolean x;
+	public static boolean y;
+	public static void main(String[] args) {
+		
+		x = false;
+		y = false;
+		
+		if(x) { //x is true
+			System.out.println("X is true");
+		} else if (y) { //y is true
+			System.out.println("Y is true");
+		} else {
+			System.out.println("Neither are true");
+		}
+		
+	}
+}
---
+++ Loops!/src/ForLoops.java
@@ -0,0 +1,9 @@
+
+public class ForLoops {
+	public static void main(String[] args) {
+		for(int i = 0; i <= 1000; i++) {
+			System.out.println(i);
+		}
+		System.out.println("Loop done");
+	}
+}
